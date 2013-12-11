4a4efc1a6d6ec2bb9605aaf3841b026a2eb00975
yancheng <zqalyc@163.com>
2013-12-11 11:20:36 +0800

    origin_master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mimas::Application.config.secret_key_base = '05d5d4b064f1944c02ea438d65b960201432e79b8c431ab7fb1f6ced773b329291842368417d84dc953a09ce500383b4d15ccec48963f532b15ade575c578412'
