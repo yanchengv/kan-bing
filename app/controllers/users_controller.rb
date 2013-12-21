ef40f652f2479b1bea4268350ec2efcef6019b5f
wangfang <wangfang_zry_0824@163.com>
2013-12-21 13:13:34 +0800

    调用接口实现个人资料的修改

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+#encoding:utf-8
 class UsersController < ApplicationController
   before_filter :signed_in_user
   def index
@@ -23,23 +24,27 @@ class UsersController < ApplicationController
     @image = '/code/code_image'
     render json: {image: @image}
   end
-=begin
+
   def profile_update
     @email=params[:@user][:email].match(/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/)
     puts @email
     p=params[:@user][:sex]
-    @sex=true
+    @sex='男'
     if p.to_s=='female'
-      @sex=false
+      @sex='女'
     end
     @js={}
     #@users=User.where(username:params[:@user][:username])
     @user0 = User.new
-    @users1 = @user0.get_req('findByName?name='+params[:@user][:username])['data']
+    @users1 = @user0.get_req('find_by_name?name='+params[:@user][:username])['data']
+    puts @user1
     @exist=false
-    if @users1.length==0
+    puts current_user['name']
+    puts @users1
+    if @users1.nil?
+      puts 'baekhyun'
       @exist=true
-    elsif @users1.length==1 && @users1['name']==current_user['name']
+    elsif !@users1.nil? && @users1['name']==current_user['name']
       @exist=true
     end
     if params[:@user][:username]=='' || params[:@user][:realname]=='' || params[:@user][:email]==''
@@ -64,20 +69,11 @@ class UsersController < ApplicationController
         format.js
       end
     else
-      #@user.update_attribute(:username, params[:@user][:username])
-      #@user.update_attribute(:realname, params[:@user][:realname])
-      #@user.update_attribute(:language, params[:@user][:language])
-      #@user.update_attribute(:address, params[:@user][:address])
-      #@user.update_attribute(:phone_number, params[:@user][:phone])
-      #@user.update_attribute(:email, params[:@user][:email])
-      #@user.update_attribute(:birthdate, params[:@user][:birthdate])
-      #@user.update_attribute(:sex, @sex)
-#todo 调用接口修改数据
-      path = ''
-      param = {}
-      @user0.post_req(path,param)
-      cookie[:remember_token] = @user['remember_token']
-      sign_in @user
+      path = 'update_profile?user_id='+current_user['id'].to_s+'&realname='+ params[:@user][:realname] + '&address=' + params[:@user][:address]+'&phone='+params[:@user][:phone]+'&email='+params[:@user][:email]+'&birtday='+ params[:@user][:birthdate]+'&gender='+@sex
+      #path1 = 'update_profile'
+      param = {'user_id' => current_user['id'],'username' => params[:@user][:username],'realname' => params[:@user][:realname],'address' => params[:@user][:address],'phone' => params[:@user][:phone],'email' => params[:@user][:email],'birthday' => params[:@user][:birthdate],'gender' => @sex}
+      @user0.put_req(path,param)
+      #@user0.post_req(path1,param)
       @js={:pd => 'true'}
       respond_to do |format|
         format.html
@@ -86,7 +82,7 @@ class UsersController < ApplicationController
       end
     end
   end
-=end
+
   def password_update
     puts session[:code]
     current_user_id=current_user['id']
@@ -103,7 +99,7 @@ class UsersController < ApplicationController
     else
       param = {'current_user_id' => current_user_id,'old_password' => params[:@user][:old_password],'new_password' => params[:@user][:new_password]}
       @user = User.new
-      @result = @user.post_req('updatePassword',param)
+      @result = @user.put_req('updatePassword?current_user_id='+current_user_id.to_s+'&old_password='+params[:@user][:old_password]+'&new_password='+params[:@user][:new_password],param)
       if @result['success']
         sign_in current_user
         @js={:pd=>'true'}
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,16 +13,19 @@ class User
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
+
+#调用post请求接口
   def post_req(path,*arg)
     uri = URI(CIS_URL+path)
     res = Net::HTTP.post_form(uri, *arg)
     puts res.body
     @search_result = JSON.parse res.body
   end
+
+#根据令牌调用接口查找当前用户
   def find_by_remember_token(remember_token)
-    path='sessions/find_user'
-    param = {'remember_token' => remember_token}
-    @search_result = self.post_req(path,param)
+    path='sessions/find_user?remember_token='+remember_token.to_s
+    @search_result = self.get_req(path)
     if @search_result['success']
       puts  @search_result['data']
       return @search_result['data']
@@ -30,10 +33,20 @@ class User
       return nil
     end
   end
+
+#调用get请求接口
   def get_req(path)
     uri = URI(CIS_URL+path)
     res = Net::HTTP.get(uri)
     @search_result = JSON.parse res
+  end
+
+#调用put请求接口
+  def put_req(path,data)
+    uri = CIS_URL+path
+    c = Curl::Easy.new(uri)
+    c.http_put(data)
+    @search_result = c.body_str
   end
 
   private
