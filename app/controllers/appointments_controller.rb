a243ea9d0e394686bb859604f7fbca4bbe86fae0
wangfang <wangfang_zry_0824@163.com>
2014-01-02 15:44:15 +0800

    医生预约安排

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,8 +1,15 @@
 class AppointmentsController < ApplicationController
+  before_filter :signed_in_user
   def myappointment
       if !current_user['doctor_id'].nil?
+        @user = User.new
+        @schedules = @user.get_req('myschedules?doctor_id='+current_user['doctor_id'].to_s)
+        @appointmentSchedules = @schedules['app_schedules']
+        @cancelrecords = @schedules['cancel_schedules']
+        @dictionary = @schedules['dictionary']
         render 'appointments/myapp'
-      else
+      elsif !current_user['patient_id'].nil?
+
       end
   end
 end
