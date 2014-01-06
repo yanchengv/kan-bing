a342058b43de5651b0ba850abfca6e0386d7053d
yancheng <zqalyc@163.com>
2014-01-06 10:05:16 +0800

    部署文件

---
+++ config/unicorn.rb
@@ -0,0 +1,14 @@
+worker_processes 1
+
+APP_PATH = "/home/ubuntu/deploy/current"
+working_directory APP_PATH
+
+#/tmp/unicorn.prometheus.sock;  这个sock来自 nginx的配置
+listen "/tmp/unicorn.mimas.sock", :backlog => 64
+#listen 8000, :tcp_nopush => true
+pid APP_PATH + "/tmp/pids/unicorn.pid"
+# By default, the Unicorn logger will write to stderr.
+# Additionally, ome applications/frameworks log to stderr or stdout,
+# so prevent them from going to /dev/null when daemonized here:
+stderr_path APP_PATH + "/log/unicorn.stderr.log"
+stdout_path APP_PATH + "/log/unicorn.stderr.log"
