5d1a574caab5388e52f1d390f22b3993c0d5b442
wangfang <wangfang_zry_0824@163.com>
2014-01-23 18:06:29 +0800

    页面的调整及分页

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,8 +4,18 @@ class PatientsController < ApplicationController
   def get_aspects
     @user = User.new
     param = {'remember_token' => current_user['remember_token']}
-    @contact_doctors = @user.post_req('trfriends.json',param)['data']
+    @cont_doctors = @user.post_req('trfriends.json',param)['data']
     @contact_main_doctors = @user.post_req('maindoc.json',param)['data']
+    @contact_doctors = []
+    if @cont_doctors.length > 9
+      i = 0
+      while i<9
+        @contact_doctors.push(@cont_doctors[i])
+        i=i+1
+      end
+    else
+      @contact_doctors = @cont_doctors
+    end
     #@friend=@user.get_req('treatment_relationships/getfriends2?patient_id='+current_user['patient_id'].to_s)['data']
     #@contact_doctors = @friend['docfriends']
     #@contact_main_doctors=@friend['doctor']
@@ -23,8 +33,18 @@ class PatientsController < ApplicationController
     if !current_user['doctor_id'].nil?
       @patient1 = @user.get_req('patients/find_patient?patient_id='+params[:id].to_s+'&remember_token='+current_user['remember_token'])['data']
       @friend=@user.get_req('treatment_relationships/getfriends2?patient_id='+params[:id].to_s+'&remember_token='+current_user['remember_token'])['data']
-      @contact_doctors1 = @friend['docfriends']
+      @cont_doctors = @friend['docfriends']
       @contact_main_doctors1=@friend['doctor']
+      @contact_doctors1 = []
+      if @cont_doctors.length > 9
+        i = 0
+        while i<9
+          @contact_doctors1.push(@cont_doctors[i])
+          i=i+1
+        end
+      else
+        @contact_doctors1 = @cont_doctors
+      end
       @is_friends = is_friends
     else
       flash[:success] = "您没有访问该用户信息的权限"
@@ -58,7 +78,29 @@ class PatientsController < ApplicationController
 
   def change_main_doctor
     @user = User.new
-    @doctor_users=@user.get_req('doctors/find_all_doctor2?remember_token='+current_user['remember_token'])['data']
+    @current_page=params[:page]
+    @doc_users=@user.get_req('doctors/find_all_doctor2?remember_token='+current_user['remember_token'])['data']
+    if @doc_users.count.to_i%5 == 0
+      @doc_count = @doc_users.count.to_i/5
+    else
+      @doc_count = @doc_users.count.to_i/5+1
+    end
+    if params[:page].nil?
+      params[:page] = 1
+    end
+    page = params[:page].to_i
+    per_page = 5
+    @doctor_users = []
+    i = 0
+    while i<per_page
+      num = (page-1)*per_page+i
+      if num < @doc_users.count.to_i
+        @doctor_users.push(@doc_users[num])
+        i=i+1
+      else
+        break
+      end
+    end
     render :template => "patients/change_main_doctor"
   end
 
