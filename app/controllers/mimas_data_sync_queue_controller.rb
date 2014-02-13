063f545db3c35ca96ba24eb7ceb787cf22aaddaf
yancheng <zqalyc@163.com>
2014-02-13 18:02:34 +0800

    修改同步

--- app/controllers/mimas_data_sync_queue_controller.rb
+++ app/controllers/mimas_data_sync_queue_controller.rb
@@ -1,10 +1,21 @@
 class MimasDataSyncQueueController < ApplicationController
-  skip_before_filter :verify_authenticity_token, :only => [:create]
+  skip_before_filter :verify_authenticity_token
+
   def create
     @data_sync=MimasDataSyncQueue.new
-    response=@data_sync.add_data(params)
-    puts response
-    #render json:'{"data":{"data1":true}}'
+    response=@data_sync.add(params)
+    render json: response
+  end
+
+  def destroy
+    @data_sync=MimasDataSyncQueue.new
+    response=@data_sync.destroy_data(params)
+    render json: response
+  end
+
+  def change
+    @data_sync=MimasDataSyncQueue.new
+    response=@data_sync.update_data params
     render json: response
   end
 end
--- app/models/mimas_data_sync_queue.rb
+++ app/models/mimas_data_sync_queue.rb
@@ -3,23 +3,47 @@ require 'net/http'
 class MimasDataSyncQueue < ActiveRecord::Base
   attr_accessible :foreign_key, :table_name, :code, :contents
 
-  #扫描表中所有的数据
-  def add_data(params)
+  #根据院内同步表，扫描表中所有的数据
+  def add(params)
     table_name=params["table_name"]
     data=params['data']
     @obj=table_name.constantize.new(data)
-     if @obj.save
-       {data:{success:true}}
-     else
-       {data:{success:false}}
-     end
+    if @obj.save
+      {data: {success: true}}
+    else
+      {data: {success: false}}
+    end
 
   end
 
-  def change_data(data)
+  #根据院内同步表修改相应表的字段
+  def update_data(params)
+    table_name=params['table_name']
+    contents=params['contents']
+    puts contents
+    id=params['foreign_key']
+    @obj=table_name.constantize
+    @obj2=@obj.find_by_id(id)
+    flag=@obj2.update_attributes(JSON.parse(contents))
+    if  flag
+      {data: {success: true}}
+    else
+      {data: {success: false}}
+    end
 
   end
-  def delte_data(data)
+
+  #根据院内同步表删除相应的数据
+  def destroy_data(params)
+    table_name=params['table_name']
+    id=params['foreign_key']
+    @obj=table_name.constantize
+    if @obj.destroy(id)
+      {data: {success: true}}
+    else
+      {data: {success: false}}
+    end
+
 
   end
 end
