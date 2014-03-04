d3ea36acf2a665d42158287e848a2352baf80d1b
yanlinpu <yanlinpu66@163.com>
2014-03-04 10:50:25 +0800

    部署

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,8 +13,8 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   ps aux |grep unicorn|grep -v grep |awk '{print $2}'|xargs kill -9 
 #   rainbows config.ru -c config/unicorn.rb -E production -D
 #   unicorn_rails -c ~/deploy/current/config/unicorn.rb -D  -E production
-set :domain, '192.168.1.14'
-set :deploy_to, '/home/git/deploy'
+set :domain, '192.168.1.40'
+set :deploy_to, '/home/ubuntu/deploy'
 set :repository, 'git@fitark.org:saturn/mimas.git'
 set :branch, 'master'
 set :term_mode, :system
@@ -23,7 +23,7 @@ set :term_mode, :system
 set :shared_paths, ['config/database.yml', 'log']
 
 # Optional settings:
-set :user, 'git'    # Username in the server to SSH to.
+set :user, 'ubuntu'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
 
 # This task is the environment that is loaded for most commands, such as
