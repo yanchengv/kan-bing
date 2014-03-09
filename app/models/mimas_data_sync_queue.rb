582d1c5a2af2f2ed741ba72b82c3b2b2cc7af97d
yancheng <zqalyc@163.com>
2014-03-09 19:22:29 +0800

    鲁棒性

--- app/models/mimas_data_sync_queue.rb
+++ app/models/mimas_data_sync_queue.rb
@@ -5,33 +5,52 @@ class MimasDataSyncQueue < ActiveRecord::Base
 
   #根据院内同步表，扫描表中所有的数据
   def add_data(params)
-    table_name=params['table_name']
-    if table_name=='Patient'
-      user_name=params['data']['name']
-      patient_id=params['data']['id']
-      @user=User.new
-      pk=@user.create_pk
-       User.create(id:pk,name:user_name,patient_id:patient_id,password:123,password_confirmation:123)
-    end
     data=params['data']
-    @obj=table_name.constantize.new(data)
-    if table_name=='UsReport'
+    table_name=params['table_name']
+    user_name=data['name']
+    pk=data['id']
+    #如果tale_name是patient,则需要添加新的User对应Patient.如果传来的Doctor则无需自定义User,院内系统已生成相应user并添加到同步表
+    if table_name=='Patient'
+      if User.find_by_id(pk)||Patient.find_by_id(pk)
+        {data: {success: false, content: '保存的患者ID已存在'}}
+      else
+        @user=User.new(id: pk, name: user_name, patient_id: pk, password: 'mimas', password_confirmation: 'mimas')
+        @obj=table_name.constantize.new(data)
+        if @user.save&&@obj.save
+          {data: {success: true, content: '保存成功'}}
+        else
+          {data: {success: false, content: '保存失败'}}
+        end
+      end
+    elsif table_name=='UsReport'
       #添加总索引表的数据
-      data2=params['data2']['data']
+      #data2=params['data2']['data']
+      data2=params['data2']
+      if InspectionReport.find_by_id(data2['id'])||UsReport.find_by_id(pk)
+        {data: {success: false, content: '保存的UsRport已存在'}}
+      else
+        @obj=table_name.constantize.new(data)
         @obj1=InspectionReport.new(data2)
         if @obj.save&&@obj1.save
-          {data: {success: true}}
+          {data: {success: true, content: '保存成功'}}
         else
-          {data: {success: false}}
+          {data: {success: false, content: '保存失败'}}
         end
-    else
-      if @obj.save
-        {data: {success: true}}
-      else
-        {data: {success: false}}
       end
-    end
 
+    else
+      if table_name.constantize.find_by_id(pk)
+        {data: {success: false, content: "保存的#{table_name data['id']}已存在"}}
+      else
+        @obj=table_name.constantize.new(data)
+        if @obj.save
+          {data: {success: true, content: '保存成功'}}
+        else
+          {data: {success: false, content: '保存失败'}}
+        end
+      end
+
+    end
 
   end
 
@@ -39,9 +58,9 @@ class MimasDataSyncQueue < ActiveRecord::Base
   def update_data(params)
     table_name=params['table_name']
     contents=params['contents']
-    id=params['foreign_key']
+    pk=params['foreign_key']
     @obj=table_name.constantize
-    @obj2=@obj.find_by_id(id)
+    @obj2=@obj.find_by_id(pk)
     flag=@obj2.update_attributes(JSON.parse(contents))
     if  flag
       {data: {success: true}}
