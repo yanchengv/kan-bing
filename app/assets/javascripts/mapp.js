b1e96ff8fef2d80defdbacaedd79d00d6625215f
yancheng <zqalyc@163.com>
2014-03-11 14:01:49 +0800

    layout

---
+++ app/assets/javascripts/mapp.js
@@ -0,0 +1,10 @@
+/**
+ * Created by git on 14-3-11.
+ */
+
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+//require jquery-fileupload/basic
+//= require js/select2
