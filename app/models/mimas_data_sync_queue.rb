9e897740a08801aaf1446b2aef1ae2a392f42081
yancheng <zqalyc@163.com>
2014-03-28 16:23:26 +0800

    字段修改

--- app/models/mimas_data_sync_queue.rb
+++ app/models/mimas_data_sync_queue.rb
@@ -61,13 +61,16 @@ class MimasDataSyncQueue < ActiveRecord::Base
     pk=params['foreign_key']
     @obj=table_name.constantize
     @obj2=@obj.find_by_id(pk)
-    flag=@obj2.update_attributes(JSON.parse(contents))
+    if @obj2
+    flag=@obj2.update_columns(JSON.parse(contents))
     if  flag
       {data: {success: true}}
     else
       {data: {success: false}}
     end
-
+    else
+      {data: {success: false}}
+     end
   end
 
   #根据院内同步表删除相应的数据
