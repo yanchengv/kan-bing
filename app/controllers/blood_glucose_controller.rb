540829b0af00e1553ba5c1e8702a28b0ae1f0d51
yancheng <zqalyc@163.com>
2014-04-14 16:17:58 +0800

    数据库查询血糖数据 ajax

--- app/controllers/blood_glucose_controller.rb
+++ app/controllers/blood_glucose_controller.rb
@@ -6,5 +6,20 @@ class BloodGlucoseController < ApplicationController
       measure_date=blood['measure_date']
       @blood_glucose=BloodGlucose.new(patient_id:patient_id,measure_value:measure_value,measure_date:measure_date)
       @blood_glucose.save
+
+      @blood_glucose=BloodGlucose.new
+      @blood_glucoses=BloodGlucose.where(patient_id:current_user.patient.id).order(measure_date: :asc)
+      @blood_data=[]
+      @blood_glucoses.each do |blood|
+        blood_glu=[blood.measure_date.strftime("%Y-%m-%d %H:%M:%S").to_time.to_i*1000,blood.measure_value.to_i]
+        @blood_data.append blood_glu
+
+      end
+    respond_to do |format|
+      format.html {render 'patients/show_blood_glucose'}
+      format.js{ render 'patients/show_blood_glucose'}
+      format.json {render 'patients/show_blood_glucose'}
+    end
+    #render json: 'patient/show_blood_glucose'
   end
 end
