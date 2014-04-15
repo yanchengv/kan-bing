327a1aa9bb13894f2a4120bf9aa2e74839e51daa
zhanglihua <zhanglihuahuahua@126.com>
2014-04-15 17:14:42 +0800

    swf config

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Mimas
     #config.assets.enabled = true
     #config.assets.paths << "#{Rails.root}/app/views/images"
     #config.assets.paths << "#{Rails.root}/app/views/uploads"
-    #config.assets.paths << Rails.root.join("app", "assets", "flash")
+    config.assets.paths << "#{Rails.root}/app/assets/flash"
     # Version of your assets, change this if you want to expire all your assets
     #config.assets.version = '1.0'
     #config.assets.initialize_on_precompile = false
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,13 @@
 worker_processes 1
 
-#APP_PATH = "/home/ubuntu/mimas_deploy/current"
-APP_PATH = "/dfs/deploy/mimas_deploy/current"
+APP_PATH = "/home/fitark/mimas"
+#APP_PATH = "/dfs/deploy/mimas_deploy/current"
 
 working_directory APP_PATH
 
 #/tmp/unicorn.prometheus.sock;  这个sock来自 nginx的配置
-listen "/tmp/unicorn.mimas.sock", :backlog => 64
-#listen 8000, :tcp_nopush => true
+#listen "/tmp/unicorn.mimas.sock", :backlog => 64
+listen 8000
 pid APP_PATH + "/tmp/pids/unicorn.pid"
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
