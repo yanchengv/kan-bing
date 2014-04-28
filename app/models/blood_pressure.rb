7c85089bc8efb58bb6a1b779e62bc0d4f491e9a1
yancheng <zqalyc@163.com>
2014-04-28 17:16:09 +0800

    血压优化

--- app/models/blood_pressure.rb
+++ app/models/blood_pressure.rb
@@ -1,4 +1,25 @@
 class BloodPressure < ActiveRecord::Base
   belongs_to :patient, :foreign_key => :patient_id
   attr_accessible :id, :patient_id,:measure_value,:measure_date
+
+  #添加血糖
+  def add_blood_pressure params
+    pressure=params
+    blood_pressure={}
+  end
+
+  #获取最近30天的血压
+  def get_blood_pressure patient_id
+    @blood_pressure=BloodPressure.new
+    @blood_pressure=BloodPressure.where("patient_id=? AND measure_date<=? AND measure_date>=?",patient_id,Date.today,Date.today-30).order(measure_date: :asc)
+    @pressure_data=[]
+    @blood_pressure.each do |pressure|
+      if !pressure.measure_date.nil?
+        blood_pres=[pressure.measure_date.strftime("%Y-%m-%d %H:%M:%S").to_time.to_i*1000,pressure.measure_value.to_i]
+        @pressure_data.append blood_pres
+      end
+    end
+    @pressure_data
+  end
+
 end
