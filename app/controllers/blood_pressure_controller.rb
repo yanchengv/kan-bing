47602e9f089830688132d4cde9ada84da1cf0f2e
yancheng <zqalyc@163.com>
2014-04-29 10:33:58 +0800

    data2

--- app/controllers/blood_pressure_controller.rb
+++ app/controllers/blood_pressure_controller.rb
@@ -16,6 +16,8 @@ class BloodPressureController < ApplicationController
 
   def show
     patient_id=current_user.patient_id
-    @pressure_data=BloodPressure.new.get_blood_pressure(patient_id)
+    pressure_data=BloodPressure.new.get_blood_pressure(patient_id)
+    @systolic_pressure_data=pressure_data[:pressure_data][:systolic_pressure_data]
+    @diastolic_pressure_data=pressure_data[:pressure_data][:diastolic_pressure_data]
   end
 end
