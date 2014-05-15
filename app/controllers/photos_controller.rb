110b93bf75dd4da018aee8b9c73060f5c6689e94
yancheng <zqalyc@163.com>
2014-05-15 14:27:12 +0800

    上传修改

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,8 +4,45 @@ class PhotosController < ApplicationController
   require 'curb'
   require 'fileutils'
 
+  #def create
+  #  tmp_path='public/'+current_user['id'].to_s+'.jpg'
+  #  tmp=params[:photo][:photo]
+  #  x=params[:x].to_i
+  #  y=params[:y].to_i
+  #  h=params[:h].to_i
+  #  w=params[:w].to_i
+  #  image = MiniMagick::Image.open(tmp.path)
+  #  image.crop "#{w}x#{h}+#{params[:x]}+#{params[:y]}"
+  #  image.resize '150x210'
+  #  image.write tmp_path
+  #  @crop_photo='/'+current_user['id'].to_s+'.jpg'
+  #  c = Curl::Easy.new(Settings.pic)
+  #  c.multipart_form_post = true
+  #  c.http_post(Curl::PostField.file('theFile', tmp_path))
+  #  response=JSON.parse(c.body_str)
+  #  @data=''
+  #  if response['files']&&response['files'][0]['name']
+  #    uuid=response['files'][0]['name']
+  #    pic_url=Settings.pic+uuid
+  #    @data={flag:true,url:pic_url}
+  #    if !current_user.doctor_id.nil?
+  #      current_user.doctor.update_attributes(photo: uuid)
+  #    elsif !current_user.patient_id.nil?
+  #      current_user.patient.update_attributes(photo: uuid)
+  #    end
+  #
+  #  else
+  #    @data={flag: false, url: ''}
+  #  end
+  #  FileUtils.remove_file tmp_path
+  #  #@data ={data:'123'}
+  #  render json: @data
+  #end
+
   def create
-    tmp_path='public/'+current_user['id'].to_s+'.jpg'
+    @user=User.new
+    uuid=@user.uuid('.jpg')
+    image_path=@user.create_folder_by_uuid(uuid)
     tmp=params[:photo][:photo]
     x=params[:x].to_i
     y=params[:y].to_i
@@ -14,24 +51,16 @@ class PhotosController < ApplicationController
     image = MiniMagick::Image.open(tmp.path)
     image.crop "#{w}x#{h}+#{params[:x]}+#{params[:y]}"
     image.resize '150x210'
-    image.write tmp_path
-    @crop_photo='/'+current_user['id'].to_s+'.jpg'
-    c = Curl::Easy.new(Settings.pic)
-    c.multipart_form_post = true
-    c.http_post(Curl::PostField.file('theFile', tmp_path))
-    response=JSON.parse(c.body_str)
+    image.write image_path
     @data=''
-    if response['files']&&response['files'][0]['name']
-      FileUtils.remove_file tmp_path
-      uuid=response['files'][0]['name']
+    if File.exist?(image_path)
       pic_url=Settings.pic+uuid
-      @data={flag:true,url:pic_url}
-      if !current_user.doctor_id.nil?
-        current_user.doctor.update_attributes(photo: uuid)
-      elsif !current_user.patient_id.nil?
-        current_user.patient.update_attributes(photo: uuid)
-      end
-
+          @data={flag:true,url:pic_url}
+          if !current_user.doctor_id.nil?
+            current_user.doctor.update_attributes(photo: image_path)
+          elsif !current_user.patient_id.nil?
+            current_user.patient.update_attributes(photo: image_path)
+          end
     else
       @data={flag: false, url: ''}
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -140,6 +140,35 @@ class User< ActiveRecord::Base
     id=(Settings.hospital_code.yuquan+time.to_s+random.to_s).to_i
     return id
   end
+
+  #上传文件生成uuid
+  def uuid(suffix)
+    uuid = UUIDTools::UUID.random_create.to_s
+    uuid = uuid.gsub('-','')+suffix
+    #uuid = uuid[1,2]+'/'+uuid[4,2]+'/'+uuid[7,2]+'/'+uuid[10,100]
+  end
+  #生成上传文件的目录
+  def create_folder_by_uuid(uuid)
+    folder='/dfs/jpg/'
+    if !File.exist?(folder)
+      Dir.mkdir(folder)
+    end
+    folder_2 = "#{folder}"+uuid[1,2]+"/"
+    if !File.exist?(folder_2)
+      Dir.mkdir(folder_2)
+    end
+    folder_3 = "#{folder_2}"+uuid[4,2]+"/"
+    if !File.exist?(folder_3)
+      Dir.mkdir(folder_3)
+    end
+    folder_4 = "#{folder_3}"+uuid[7,2]+"/"
+    if !File.exist?(folder_4)
+      Dir.mkdir(folder_4)
+    end
+    #image_path=/dfs/6f/c7/dc/bd4e17bad466d6f1a5b2f9.jpg
+    image_path = folder_4+uuid[10,100] + ".jpg"
+    image_path
+  end
   private
   def create_remember_token
     #Create the token
