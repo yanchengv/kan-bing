996ab001df7315ba579c557dc6e0ebd08f991276
wangfang <wangfang_zry_0824@163.com>
2014-08-08 17:51:45 +0800

    在线留言相关部分接口

---
+++ app/models/leave_message.rb
@@ -0,0 +1,6 @@
+class LeaveMessage < ActiveRecord::Base
+  belongs_to :user, :foreign_key => :user_id
+  has_many :user_replies, dependent: :destroy
+  has_many :message_likes, dependent: :destroy
+  attr_accessible :id,:user_id,:title,:content,:message_type,:view_count,:like_count,:reply_count
+end
