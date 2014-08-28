f9e38d38f1f5491abf8e9d3acddff1a0f4ceda65
闫成 <zqalyc@163.com>
2014-08-28 11:13:23 +0800

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ working_directory APP_PATH
 
 #/tmp/unicorn.prometheus.sock;  这个sock来自 nginx的配置
 listen "/tmp/unicorn.mimas.sock", :backlog => 64 #这个sock来自 nginx的配置
-listen 8009
+#listen 8009
 pid APP_PATH + "/tmp/pids/unicorn.pid"
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
