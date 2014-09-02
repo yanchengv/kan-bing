7edae896200ff8c0a026a9c0152d337d964cc6db
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-09-02 22:19:53 +0800

    1 add  attr_accessible 2 page style

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,3 @@
 class Note < ActiveRecord::Base
+  attr_accessible :head, :subhead, :content, :archtype, :deleted_at, :is_public, :is_top, :is_allow_comment, :is_visible, :who_deleted, :created_by, :created_by_id, :update_by, :update_by_id, :audit_by, :audit_time, :pubstatus, :excellent, :indexpage, :site, :pageview, :replies_count
 end
