b82049d9582758348927e4c059e38448cf590be2
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-09-21 05:20:30 +0800

    share msg

--- app/models/share.rb
+++ app/models/share.rb
@@ -6,8 +6,8 @@ class Share < ActiveRecord::Base
   validates :note_id, uniqueness: { scope: :share_user_id,message: "已经为患者分享过该内容" }
   attr_accessible :note_id, :from_user_id, :from_user_name, :share_user_id, :share_user_name, :share_type, :link
   def notice_user
-    shareMsg = ""
-    shareMsg = self.from_user_name << " 给您分享了一篇文章 " #<< "/notes/"<< self.note_id.to_s
+    doc_name = self.from_user_name
+    shareMsg =  doc_name + " 给您分享了一篇文章 "
     #create msg tip
     Notification.create(user_id: self.share_user_id,
                         code: 11, content: self.note_id,
