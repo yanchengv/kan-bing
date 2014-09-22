4f35f6f13f0d86997fa7f706aacbbf7dc438e50f
Yang Xiaofeng <yxf_cat@hotmail.com>
2014-09-22 10:28:35 +0800

    咨询调整

--- app/models/consult_question.rb
+++ app/models/consult_question.rb
@@ -1,6 +1,6 @@
 class ConsultQuestion < ActiveRecord::Base
   has_many :consult_results, :foreign_key => :consult_id, :dependent => :destroy
-  belongs_to :user_creates, :class_name => 'User', :foreign_key => "created_by" #咨询人
-  belongs_to :consult_bys, :class_name => 'User', :foreign_key => "consulting_by" #被咨询人
+  belongs_to :user_create, :class_name => 'User', :foreign_key => "created_by" #咨询人
+  belongs_to :consult_by, :class_name => 'User', :foreign_key => "consulting_by" #被咨询人
   attr_accessible :consult_content, :consulting_by, :created_by, :consult_identity
 end
