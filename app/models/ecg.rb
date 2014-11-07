29603c58f91484ed43a8623d350d635a4c7e7f52
yancheng <zqalyc@163.com>
2014-11-07 19:45:44 +0800

    ecg

--- app/models/ecg.rb
+++ app/models/ecg.rb
@@ -9,7 +9,7 @@ def inspection_report_ecg
 
   inspection_report={}
       inspection_report[:patient_id]=self.patient_id
-      inspection_report[:thumbnail]=self.id
+      inspection_report[:child_id]=self.id
       inspection_report[:parent_type]=self.parent_type
       inspection_report[:child_type]=self.child_type
       inspection_report[:hospital]=self.hospital
@@ -21,6 +21,6 @@ def inspection_report_ecg
 end
 
 def  delete_inspection_report_ecg
-  @inspection_report=InspectionReport.where(patient_id:self.patient_id,parent_type:self.parent_type,child_type: self.child_type,thumbnail:self.id).first
+  @inspection_report=InspectionReport.where(patient_id:self.patient_id,parent_type:self.parent_type,child_type: self.child_type,child_id:self.id).first
   @inspection_report.destroy
 end
