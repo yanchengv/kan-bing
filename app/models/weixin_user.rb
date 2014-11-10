14e1140c1be15c02549efac0bde6a5f71e477b29
yanlinpu <yanlinpu66@163.com>
2014-11-10 18:46:11 +0800

    微信客服消息和模板消息

--- app/models/weixin_user.rb
+++ app/models/weixin_user.rb
@@ -2,44 +2,62 @@
 require 'json'
 class WeixinUser < ActiveRecord::Base
   attr_accessible :id, :openid, :patient_id, :doctor_id, :created_at, :updated_at
-
+  #根据患者id发送客服消息
   def send_message_to_weixin(type,id,message='您好')
-    @wxus = (type=='patient') ? WeixinUser.where('patient_id=?',id) : WeixinUser.where('doctor_id=?',id)
+    #@wxus = (type=='patient') ? WeixinUser.where('patient_id=?',id) : WeixinUser.where('doctor_id=?',id)
+    @wxus = WeixinUser.where('patient_id=?',id)
     res = 'false'
     if @wxus.size != 0
       openid = @wxus.first.openid
-      #url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx814c2d89e8970870&secret=751cdb39cb83f3f387d06bde23159897'
-      url = Settings.weixin.access_token + 'appid=' + Settings.weixin.app_id + '&secret=' + Settings.weixin.app_secret
-      uri = URI.parse(url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new(uri.request_uri)
-      response = http.request(request)
-      @data = JSON.parse response.body
-      access_token = @data["access_token"]
-      #url = 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token='+access_token
-      uri = URI.parse(Settings.weixin.send_message + access_token)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      req = Net::HTTP::Post.new(uri.request_uri)
-      body = {
-          touser: openid,
-          msgtype: "text",
-          text: {
-              content: message
-          }
-      }
-      req.body = body.to_json
-      response = http.request(req)
-      res = (JSON.parse response.body)["errmsg"]
+      res = sendByOpenId(openid,message)
     end
     res
-
+  end
+  #根据open_id发送客服消息
+  def sendByOpenId(openid,message='您好')
+    access_token = getAccessToken
+    sendText(access_token,openid,message)
+  end
+  #根据open_id发送模板消息
+  def sendTmpMesByOpenId(open_id)
+    access_token = getAccessToken
+    body =  {
+        "touser"=>open_id,
+        "template_id"=>"L9JQbPqr_d218GIyn0SU4sROUP4QvwuuCeUADcwQIEo",
+        "url"=>"http://www.kanbing365.com",
+        "topcolor"=>"#FF0000",
+        "data"=>{
+            "first"=> {
+                "value"=>"您好，您已成功消费。",
+                "color"=>"#0A0A0A"
+            },
+            "keynote1"=>{
+                "value"=>"巧克力",
+                "color"=>"#CCCCCC"
+            },
+            "keynote2"=> {
+                "value"=>"39.8元",
+                "color"=>"#CCCCCC"
+            },
+            "keynote3"=>{
+                "value"=>"巧克力",
+                "color"=>"#CCCCCC"
+            },
+            "keynote4"=> {
+                "value"=>"39.8元",
+                "color"=>"#CCCCCC"
+            },
+            "remark"=> {
+                "value"=>"欢迎再次购买。",
+                "color"=>"#173177"
+            }
+        }
+    }
+    sendTmpText(access_token,body)
   end
 
-  def sendByOpenId(openid,message='您好')
+  #获取access_token
+  def getAccessToken
     url = Settings.weixin.access_token + 'appid=' + Settings.weixin.app_id + '&secret=' + Settings.weixin.app_secret
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
@@ -48,13 +66,22 @@ class WeixinUser < ActiveRecord::Base
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
     @data = JSON.parse response.body
-    access_token = @data["access_token"]
-    #url = 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token='+access_token
-    uri = URI.parse(Settings.weixin.send_message + access_token)
+    @data["access_token"]
+  end
+  #微信API post请求
+  def sendByNetHttp(url, send_body)
+    uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     req = Net::HTTP::Post.new(uri.request_uri)
+    body = send_body
+    req.body = body.to_json
+    response = http.request(req)
+    res = (JSON.parse response.body)["errmsg"]
+  end
+  #发送消息
+  def sendText(access_token,openid,message)
     body = {
         touser: openid,
         msgtype: "text",
@@ -62,8 +89,10 @@ class WeixinUser < ActiveRecord::Base
             content: message
         }
     }
-    req.body = body.to_json
-    response = http.request(req)
-    res = (JSON.parse response.body)["errmsg"]
+    sendByNetHttp(Settings.weixin.send_message + access_token,body)
+  end
+  #发送模板消息
+  def sendTmpText(access_token,body)
+    sendByNetHttp("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=" + access_token, body)
   end
 end
