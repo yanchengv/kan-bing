c839d37b914a42966ac6c1db8f9e50522c5f118e
Yang Xiaofeng <yxf_cat@hotmail.com>
2014-11-24 13:05:27 +0800

    与模版的应用有关

--- app/models/page_block.rb
+++ app/models/page_block.rb
@@ -1,5 +1,20 @@
 class PageBlock < ActiveRecord::Base
+#  belongs_to :home_page, :foreign_key => :page_id
   include SessionsHelper
-  attr_accessible :id, :name, :content, :created_id, :created_name, :updated_id, :updated_name, :hospital_id, :hospital_name, :department_id, :department_name, :page_id, :position, :is_show
+  before_create :set_pk_code
+  # before_save :set_default_value
+  def set_pk_code
+    if self.id&&self.id!=0
+      self.id = id
+    else
+      self.id = pk_id_rules
+    end
+  end
 
+  def set_default_value
+    @hospital = Hospital.where(:id => self.hospital_id)
+    self.hospital_name = @hospital.first.name if !@hospital.first.nil?
+    @department = Department.where(:id => self.department_id)
+    self.department_name = @department.first.name if !@department.first.nil?
+  end
 end
