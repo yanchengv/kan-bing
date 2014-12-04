3514f3feb64ea2b23ea3021926217b9a9d3163c8
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-12-04 09:48:28 +0800

    user加入group后更新相关字段

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,9 +1,15 @@
 class GroupUser < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
+  belongs_to :doctor
   validates :user_id, uniqueness: { scope: :group_id,
                                  message: "已经加入过该机构" }
 
-  attr_accessible :group_id ,:user_id
+  attr_accessible :group_id ,:user_id ,:doctor_id ,:doctor_name
 
+  after_create :haddler_save
+
+  def haddler_save
+    update_attributes(doctor_id: user.doctor_id ,doctor_name:user.doctor.name)
+  end
 end
