94a511e6603574dbaeeffd23641c81efa5017883
yancheng <zqalyc@163.com>
2014-12-05 18:46:18 +0800

    新闻显示

--- app/controllers/block_content_controller.rb
+++ app/controllers/block_content_controller.rb
@@ -1,7 +1,10 @@
 class BlockContentController < ApplicationController
   def show
       id=params[:content_id]
-      @block_content=BlockContent.where(id:id).only(:block_name, :title, :content).first#.order(create_date: :asc)
+      @block_content=BlockContent.where(id:id).first#.order(create_date: :asc)
+      @page_block=PageBlock.where(id:@block_content.block_id).first
+      p  @page_block.id
+      render template:'block_content/show'
   end
   private
   # Never trust parameters from the scary internet, only allow the white list through.
