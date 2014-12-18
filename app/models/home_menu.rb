6517bba6840aa1bbdb386eb67c8c59ead3e92599
Yang Xiaofeng <yxf_cat@hotmail.com>
2014-12-18 18:29:40 +0800

    没有注册的医生显示,导航

--- app/models/home_menu.rb
+++ app/models/home_menu.rb
@@ -1,3 +1,5 @@
 class HomeMenu < ActiveRecord::Base
+  belongs_to :parent_menu, foreign_key: :parent_id, class_name: "HomeMenu"
+  has_many :child_menus, foreign_key: :parent_id, class_name: "HomeMenu", dependent: :destroy
   attr_accessible  :name, :parent_id, :hospital_id, :department_id, :show_in_menu, :link_url, :skip_to_first_child, :show_in_header, :show_in_footer, :depth, :content
 end
