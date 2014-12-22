fe88286edb37d25f085736089e87dafe16b685ba
Yang Xiaofeng <yxf_cat@hotmail.com>
2014-12-22 16:49:24 +0800

    导航

--- app/helpers/center_helper.rb
+++ app/helpers/center_helper.rb
@@ -8,4 +8,30 @@ module CenterHelper
         end
        return  @logo
   end
+
+  def get_menus_by_host host
+    @domain=Domain.where(name: host).first
+    if @domain
+      str = ''
+      home_menus = HomeMenu.where('hospital_id=? and department_id=? and show_in_menu = ? and parent_id = 0', 1, 1, true).order(depth: :asc)
+      str << get_navigations(home_menus)
+      return str
+    else
+      return ''
+    end
+  end
+
+  def get_navigations(home_menus)
+    str = ''
+      home_menus.each do |hm|
+        str << "<li class='current'><a href='/home_menu/show/#{hm.id}'>#{hm.name}</a>"
+        if !hm.child_menus.nil? && !hm.child_menus.empty?
+          str << "<ul>"
+          str << get_navigations(hm.child_menus)
+          str << "</ul>"
+        end
+        str << "</li>"
+      end
+      return str
+  end
 end
