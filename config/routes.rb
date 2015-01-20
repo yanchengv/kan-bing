fc2e3652854695d007f662636d5389e1e317b766
yancheng <zqalyc@163.com>
2015-01-20 16:33:46 +0800

    医嘱修改

--- config/routes.rb
+++ config/routes.rb
@@ -735,6 +735,7 @@ Mimas::Application.routes.draw do
       post 'teller',to:'diagnose_treat#teller'
       post 'diagnose',to:'diagnose_treat#diagnose'
       post 'doctor_order_create',to:'diagnose_treat#doctor_order_create'
+      get 'destroy_doctor_order',to:'diagnose_treat#destroy_doctor_order'
     end
   end
   # Example of regular route:
