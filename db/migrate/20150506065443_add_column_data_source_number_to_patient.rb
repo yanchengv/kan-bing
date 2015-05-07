710226a10fdbd4584678b537645a70222b4c9fb7
wangfang <wangfang_zry_0824@163.com>
2015-05-07 16:50:10 +0800

    migrate bug

--- db/migrate/20150506065443_add_column_data_source_number_to_patient.rb
+++ db/migrate/20150506065443_add_column_data_source_number_to_patient.rb
@@ -2,6 +2,6 @@ class AddColumnDataSourceNumberToPatient < ActiveRecord::Migration
   def change
     add_column :patients, :data_source_number, :string
     add_column :inspection_reports, :data_source_number, :string
-    add_column :inspection_ultrasounds, :data_source_number, :string
+    # add_column :inspection_ultrasounds, :data_source_number, :string
   end
 end
--- db/migrate/20150507080432_change_column_type_data_source_number_to_text.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnTypeDataSourceNumberToText < ActiveRecord::Migration
-  def change
-    change_column :inspection_ultrasounds, :data_source_number, :text
-  end
-end
